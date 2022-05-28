f7767bc1bd02fa95dd8fee2cd872dd5c40c0441a
Min Song <min.song@bagelcode.com>
2022-05-29 01:47:23 +0900

    Add scene, asset reference find

--- src/loader.ts
+++ src/loader.ts
@@ -4,11 +4,11 @@ import path = require("path");
 import { outputLog } from './logger';
 import { getGuid } from './parser';
 
-let prefabs: string[];
+let files: string[];
 
 export function syncUnityFiles(dirPath: string) {
     outputLog("Start unity files sync");
-    prefabs = sync(dirPath, []);
+    files = sync(dirPath, []);
     vscode.window.showInformationMessage("Finish unity files sync");
     outputLog("Finish unity files sync");
 }
@@ -23,9 +23,9 @@ export function findPrefabReference() {
     let metaFile = fs.readFileSync(`${file + '.meta'}`, { encoding: "utf8" });
     let guid = getGuid(metaFile);
 
-    outputLog(`${path.basename(file)} prefab reference list`);
+    outputLog(`${path.basename(file)} reference list`);
     
-    prefabs.forEach(prefab => {
+    files.forEach(prefab => {
         if (fs.readFileSync(prefab).includes(guid)) {
             outputLog(prefab);
         }
@@ -38,9 +38,10 @@ function sync(dirPath: string, arrayOfFiles: string[]) {
     arrayOfFiles = arrayOfFiles;
   
     files.forEach(file => {
+        let extname = path.extname(file);
         if (fs.statSync(dirPath + "/" + file).isDirectory()) {
             arrayOfFiles = sync(dirPath + "/" + file, arrayOfFiles);
-        } else if (path.extname(file) === '.prefab') {
+        } else if (extname === '.prefab' || extname === '.asset' || extname === '.unity') {
             arrayOfFiles.push(path.join(dirPath, "/", file));
         }
     });
