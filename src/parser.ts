b49ead2e331301c8e8469c7a600b242a247bde83
Elik Song <elik@oxygen.ist>
2024-02-16 19:15:52 +0900

    Add project parser

--- src/parser.ts
+++ src/parser.ts
@@ -8,3 +8,13 @@ export function getGuid(text: string) {
 
     return guid.split(':')[1].trim();
 }
+
+export function getProjectName(data: string) {
+    let regex = /productName: (.\w)+/;
+    return data.match(regex)?.[1] ?? "";
+}
+
+export function getProjectVersion(data: string) {
+    let regex = /m_EditorVersionWithRevision:: (.\w)+/;
+    return data.match(regex)?.[1] ?? "";
+}
