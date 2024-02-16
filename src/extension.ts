e0a72b0f06f7a901e2a568b8d101407750ac6e5a
Elik Song <elik@oxygen.ist>
2024-02-16 19:16:15 +0900

    Remove main extension code

--- src/extension.ts
+++ src/extension.ts
@@ -5,31 +5,6 @@ const path = require('path');
 
 export function activate(context: vscode.ExtensionContext) {
 	new MetaExplorer(context);
-
-	const cloverProvider = new CloverViewProvider(context.extensionUri);
-    vscode.window.registerWebviewViewProvider('clover.mainView', cloverProvider);
-    vscode.commands.executeCommand('workbench.view.extension.clover-activitybar');
 	initialize(context);
-}
-
-class CloverViewProvider implements vscode.WebviewViewProvider {
-    private _extensionUri: vscode.Uri;
-
-    constructor(extensionUri: vscode.Uri) {
-        this._extensionUri = extensionUri;
-    }
-
-    resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext<unknown>, token: vscode.CancellationToken) {
-        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-    }
-
-    private _getHtmlForWebview(webview: vscode.Webview) {
-        return `
-            <html>
-            <body>
-                <h1>Clover Webview</h1>
-				I m h e r e
-            </body>
-            </html>`;
-    }
+    vscode.commands.executeCommand('workbench.view.extension.clover-activitybar');
 }
