bd9f89cf8b984813721fe8414ed80dc2a40f3c42
Elik Song <elik@oxygen.ist>
2024-02-16 19:21:07 +0900

    Edit url to string path

--- src/view/mainViewProvider.ts
+++ src/view/mainViewProvider.ts
@@ -1,18 +1,19 @@
 import * as vscode from 'vscode';
 import * as parser from '../parser';
 import fs = require('fs');
+import path = require('path');
 
 export class MainViewProvider implements vscode.WebviewViewProvider {
     private _extensionUri: vscode.Uri;
     private _projectName: string;
     private _projectVersion: string;
 
-    constructor(extensionUri: vscode.Uri, projectPath: vscode.Uri) {
+    constructor(extensionUri: vscode.Uri, projectPath: string) {
         this._extensionUri = extensionUri;
-        var projectSettingsPath = vscode.Uri.joinPath(projectPath, "ProjectSettings", "ProjectSettings.asset");
-        var projectVersionPath = vscode.Uri.joinPath(projectPath, "ProjectSettings", "ProjectVersion.txt");
-        this._projectName = parser.getProjectName(fs.readFileSync(projectSettingsPath.path, { encoding: 'utf8' }));
-        this._projectVersion = parser.getProjectVersion(fs.readFileSync(projectVersionPath.path, { encoding: 'utf8' }));
+        var projectSettingsPath = path.join(projectPath, "ProjectSettings", "ProjectSettings.asset");
+        var projectVersionPath = path.join(projectPath, "ProjectSettings", "ProjectVersion.txt");
+        this._projectName = parser.getProjectName(fs.readFileSync(projectSettingsPath, { encoding: 'utf8' }));
+        this._projectVersion = parser.getProjectVersion(fs.readFileSync(projectVersionPath, { encoding: 'utf8' }));
 
     }
 
